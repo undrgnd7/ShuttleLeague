40dc1e0b16e0194278b6a75a5e1f7391fc6e53d9
undrgnd7 <undrgnd7@gmail.com>
2026-07-01 04:15:16 +0800

    Create attendance_repository.dart

---
+++ lib/features/attendance/domain/attendance_repository.dart
@@ -0,0 +1,13 @@
+import '../domain/session_model.dart';
+
+abstract class AttendanceRepository {
+  Future<String> createSession(String leagueId);
+
+  Future<void> checkIn({
+    required String sessionId,
+    required String leagueId,
+    required String playerId,
+  });
+
+  Future<List<String>> getCheckedInPlayers(String sessionId);
+}
