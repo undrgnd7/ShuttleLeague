bfc6ef1840196fa3a005ef117183d182d854a7a2
undrgnd7 <undrgnd7@gmail.com>
2026-07-01 04:15:34 +0800

    Create attendance_repository_impl.dart

---
+++ lib/features/attendance/data/attendance_repository_impl.dart
@@ -0,0 +1,46 @@
+import 'package:drift/drift.dart';
+import 'package:uuid/uuid.dart';
+
+import '../../../../core/database/app_database.dart';
+import '../domain/session_model.dart';
+import '../../domain/attendance_repository.dart';
+
+class AttendanceRepositoryImpl implements AttendanceRepository {
+  final AppDatabase db;
+
+  AttendanceRepositoryImpl(this.db);
+
+  @override
+  Future<String> createSession(String leagueId) async {
+    final sessionId = const Uuid().v4();
+
+    return sessionId;
+  }
+
+  @override
+  Future<void> checkIn({
+    required String sessionId,
+    required String leagueId,
+    required String playerId,
+  }) async {
+    await db.into(db.attendance).insert(
+      AttendanceCompanion.insert(
+        id: const Uuid().v4(),
+        leagueId: leagueId,
+        playerId: playerId,
+        sessionId: sessionId,
+        checkInTime: DateTime.now(),
+      ),
+      mode: InsertMode.insertOrReplace,
+    );
+  }
+
+  @override
+  Future<List<String>> getCheckedInPlayers(String sessionId) async {
+    final rows = await (db.select(db.attendance)
+          ..where((a) => a.sessionId.equals(sessionId)))
+        .get();
+
+    return rows.map((e) => e.playerId).toList();
+  }
+}
