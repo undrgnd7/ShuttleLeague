d405f828800672b068b836d586da0e81a7fcae56
undrgnd7 <undrgnd7@gmail.com>
2026-07-01 04:14:58 +0800

    Create session_model.dart

---
+++ lib/features/attendance/domain/session_model.dart
@@ -0,0 +1,11 @@
+class SessionModel {
+  final String id;
+  final String leagueId;
+  final DateTime createdAt;
+
+  SessionModel({
+    required this.id,
+    required this.leagueId,
+    required this.createdAt,
+  });
+}
