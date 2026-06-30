c299f10efd0236244124c3a50e4015fb3bf811c1
undrgnd7 <undrgnd7@gmail.com>
2026-07-01 04:01:42 +0800

    Create league_model.dart

---
+++ lib/features/league/data/league_model.dart
@@ -0,0 +1,13 @@
+class LeagueModel {
+  final String id;
+  final String name;
+  final int maxPlayers;
+  final DateTime createdAt;
+
+  LeagueModel({
+    required this.id,
+    required this.name,
+    required this.maxPlayers,
+    required this.createdAt,
+  });
+}
