9e5bff561e97cadeb32e8d3862e94cd6e9562028
undrgnd7 <undrgnd7@gmail.com>
2026-07-01 03:42:46 +0800

    Create player_repository_impl.dart

---
+++ lib/features/player/data/player_repository_impl.dart
@@ -0,0 +1,39 @@
+import 'package:drift/drift.dart';
+import '../../../../core/database/app_database.dart';
+import '../domain/player_repository.dart';
+import 'player_model.dart';
+
+class PlayerRepositoryImpl implements PlayerRepository {
+  final AppDatabase db;
+
+  PlayerRepositoryImpl(this.db);
+
+  @override
+  Future<void> addPlayer(PlayerModel player) async {
+    await db.into(db.players).insert(
+      PlayersCompanion.insert(
+        id: player.id,
+        name: player.name,
+        skillLevel: player.skillLevel,
+        createdAt: player.createdAt,
+      ),
+      mode: InsertMode.insertOrReplace,
+    );
+  }
+
+  @override
+  Future<List<PlayerModel>> getPlayers() async {
+    final rows = await db.select(db.players).get();
+
+    return rows
+        .map(
+          (p) => PlayerModel(
+            id: p.id,
+            name: p.name,
+            skillLevel: p.skillLevel,
+            createdAt: p.createdAt,
+          ),
+        )
+        .toList();
+  }
+}
