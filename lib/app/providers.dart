69ecaf8c51f63e415d4bd80496ba2f1d522e8733
undrgnd7 <undrgnd7@gmail.com>
2026-07-01 03:29:55 +0800

    Create providers.dart

---
+++ lib/app/providers.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// Global app state providers (to be expanded later)
+
+final appInitProvider = Provider<bool>((ref) {
+  return true;
+});
