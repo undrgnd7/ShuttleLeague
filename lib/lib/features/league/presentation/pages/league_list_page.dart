8e5ab4c0060d8ed6b528e5cfa474e4a5dee91220
undrgnd7 <undrgnd7@gmail.com>
2026-07-01 03:32:55 +0800

    Create league_list_page.dart

---
+++ lib/lib/features/league/presentation/pages/league_list_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LeagueListPage extends StatelessWidget {
+  const LeagueListPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Leagues')),
+      body: const Center(
+        child: Text('League system coming next commit'),
+      ),
+    );
+  }
+}
