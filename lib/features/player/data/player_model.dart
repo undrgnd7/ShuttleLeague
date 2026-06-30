1bc1fbeb262dc26441156eb3d783e049bf79d73c
undrgnd7 <undrgnd7@gmail.com>
2026-07-01 03:42:31 +0800

    Create player_model.dart

---
+++ lib/features/player/data/player_model.dart
@@ -0,0 +1,13 @@
+class PlayerModel {
+  final String id;
+  final String name;
+  final int skillLevel;
+  final DateTime createdAt;
+
+  PlayerModel({
+    required this.id,
+    required this.name,
+    required this.skillLevel,
+    required this.createdAt,
+  });
+}
