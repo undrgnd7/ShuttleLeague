8d69246c2215c1acc4daabe461728e6004c6bede
undrgnd7 <undrgnd7@gmail.com>
2026-07-01 04:08:00 +0800

    Update league_repository.dart

--- lib/features/league/domain/league_repository.dart
+++ lib/features/league/domain/league_repository.dart
@@ -1,6 +1,15 @@
+import '../../player/data/player_model.dart';
 import '../data/league_model.dart';
 
 abstract class LeagueRepository {
   Future<List<LeagueModel>> getLeagues();
   Future<void> createLeague(LeagueModel league);
+
+  // NEW
+  Future<void> addPlayerToLeague({
+    required String leagueId,
+    required PlayerModel player,
+  });
+
+  Future<List<PlayerModel>> getLeaguePlayers(String leagueId);
 }
