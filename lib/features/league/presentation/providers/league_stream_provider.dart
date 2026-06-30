c89eddfbd2afe70023358f649666a6d5d4cf884f
undrgnd7 <undrgnd7@gmail.com>
2026-07-01 04:27:30 +0800

    Create league_stream_provider.dart

---
+++ lib/features/league/presentation/providers/league_stream_provider.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../../../../core/firebase/firebase_provider.dart';
+
+final leagueStreamProvider =
+    StreamProvider.family<List<Map<String, dynamic>>, String>(
+  (ref, leagueId) {
+    final firestore = ref.read(firestoreProvider);
+
+    return firestore
+        .collection('leagues')
+        .doc(leagueId)
+        .collection('players')
+        .snapshots()
+        .map((snapshot) {
+      return snapshot.docs.map((d) => d.data()).toList();
+    });
+  },
+);
